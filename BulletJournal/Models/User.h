c579ca962155c8f0633595ece9ce76ee4788fb34
Tomisin Ogunfunmi <tomisin@fb.com>
2021-07-15 18:11:26 -0500

    Added user model

---
+++ BulletJournal/Models/User.h
@@ -0,0 +1,19 @@
+//
+//  User.h
+//  BulletJournal
+//
+//  Created by tomisin on 7/15/21.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface User : NSObject
+
+// MARK: Properties
+@property (nonatomic, strong) NSString *name;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BulletJournal/Models/User.m
@@ -0,0 +1,12 @@
+//
+//  User.m
+//  BulletJournal
+//
+//  Created by tomisin on 7/15/21.
+//
+
+#import "User.h"
+
+@implementation User
+
+@end
