f92a73903ca79c413b00fa71bc99c691f451f587
Tomisin Ogunfunmi <tomisin@fb.com>
2021-07-26 15:39:48 -0500

    Added todo query for listing under calendar

--- BulletJournal/View Controllers/CalendarViewController.m
+++ BulletJournal/View Controllers/CalendarViewController.m
@@ -9,6 +9,7 @@
 
 #import "FSCalendar.h"
 #import "CoreLocation/CoreLocation.h"
+#import "Parse/Parse.h"
 
 @interface CalendarViewController () <FSCalendarDelegate, CLLocationManagerDelegate>
 
@@ -19,6 +20,8 @@
 @property (weak, nonatomic) IBOutlet UILabel *dateLabel;
 @property (weak, nonatomic) IBOutlet UILabel *dayPreview;
 @property (strong, nonatomic) CLLocationManager *locationManager;
+@property (strong, nonatomic) NSString *latitude;
+@property (strong, nonatomic) NSString *longitude;
 
 @end
 
@@ -32,6 +35,8 @@
     self.locationManager = [[CLLocationManager alloc] init];
     self.locationManager.delegate = self;
     [self.locationManager requestWhenInUseAuthorization]; //non-blocking call
+    
+    
 }
 
 - (void)calendar:(FSCalendar *)calendar didSelectDate:(NSDate *)date atMonthPosition:(FSCalendarMonthPosition)monthPosition{
@@ -40,13 +45,43 @@
     NSString *dateString = [formatter stringFromDate:date];
     NSLog(@"%@", dateString);
     self.dateLabel.text = dateString;
-    //TODO: do a query to load the bottom half of the days
+    
+    
+    
+    //Query to load the bottom half of the days
+    //TODO: similar method as fetchPosts in DailyTodoViewController
+    //TODO: filter for only the day selected
+    self.dayPreview.text = @"";
+    PFQuery *query = [PFQuery queryWithClassName:@"Bullet"];
+    query.limit = 20;
+    [query orderByDescending:@"createdAt"];
+    [query findObjectsInBackgroundWithBlock:^(NSArray *posts, NSError *error){
+        if (posts != nil) {
+            unsigned long i, cnt = [posts count];
+            for(i = 0; i < cnt; i++)
+            {
+                NSString *string = [posts objectAtIndex:i][@"Description"];
+                string = [string stringByAppendingString:@"\n"];
+                NSString *dayPreviewString = [self.dayPreview.text stringByAppendingString:string];
+                self.dayPreview.text = dayPreviewString;
+            }
+            
+        }
+        else {
+            NSLog(@"%@", error.localizedDescription);
+        }
+    }];
+    
 }
 
 - (void)locationManagerDidChangeAuthorization:(CLLocationManager *)manager {
     if (manager.authorizationStatus == kCLAuthorizationStatusAuthorizedWhenInUse ||
         manager.authorizationStatus == kCLAuthorizationStatusAuthorizedAlways){
+        CLLocationCoordinate2D coordinate = [[self.locationManager location] coordinate];
+        self.latitude = [NSString stringWithFormat:@"%f", coordinate.latitude];
+        self.longitude = [NSString stringWithFormat:@"%f", coordinate.longitude];
         NSLog(@"%@", [self.locationManager location]);
+
     }
 }
 
